bd636bce97463d2dedbfc2ac746a118b76d0ae94
kphmd <ikphmd@gmail.com>
2020-08-16 15:55:35 +0800

    HW2.py

--- HW2/HW2.py
+++ HW2/HW2.py
@@ -4,12 +4,14 @@ import math
 
 # ti.core.toggle_advance_optimization(False)
 # ti.init(debug=True)
-ti.init(arch=ti.gpu)
+# ti.init(arch=ti.gpu)
+ti.init(arch=ti.cpu)
 
 n_dim = 2
-dt    = 1e-2
+n_ptr = 8
+dt    = 1e-3
 ht    = dt * 0.5 # half of dt
-max_num_particles = 256
+max_num_particles = 1024
 connection_radius = 0.9
 
 bottom_y      = 0.05
@@ -27,12 +29,15 @@ paused           = ti.field(ti.i32, shape=())
 
 # rest_length[i, j] = 0 means i and j are not connected
 rest_length  = ti.field(ti.f32, shape=(max_num_particles, max_num_particles))
-deform_length  = ti.field(ti.f32, shape=(max_num_particles, max_num_particles))
+num_ptr      = ti.field(ti.i32, shape=())
+adj_ptr      = ti.field(ti.i32, shape=(max_num_particles, n_ptr))
+# rest_length  = ti.root.dense(ti.ij, max_num_particles)
 position     = ti.Vector.field(n_dim, dtype=ti.f32, shape=max_num_particles) # position
 velocity     = ti.Vector.field(n_dim, dtype=ti.f32, shape=max_num_particles) # velocity
 new_velocity = ti.Vector.field(n_dim, dtype=ti.f32, shape=max_num_particles)
 force        = ti.Vector.field(n_dim, dtype=ti.f32, shape=max_num_particles) # force 
 new_force    = ti.Vector.field(n_dim, dtype=ti.f32, shape=max_num_particles) 
+out_force    = ti.Vector.field(n_dim, dtype=ti.f32, shape=max_num_particles) 
 
 # A @ new_velocity = b
 A      = ti.Matrix.field(n_dim, n_dim, dtype=ti.f32, shape=(max_num_particles, max_num_particles))
@@ -42,11 +47,12 @@ new_dv = ti.Vector.field(n_dim, dtype=ti.f32, shape=max_num_particles)
 
 resi_out = ti.field(ti.f32,shape=(2))
 
-gravity[None]          = [0, -2.8]
+num_ptr[None]          = n_ptr
+gravity[None]          = [0, 0] # -9.8]
 num_particles[None]    = 0
 particle_mass[None]    = 1
 damping[None]          = 0
-spring_stiffness[None] = 30000
+spring_stiffness[None] = 300
 paused[None]           = False
 
 @ti.func
@@ -55,10 +61,12 @@ def iterate():
     for i in range(n):
         r[i] = b[i] * 1.0
         
-        for j in range(n):
-            if i != j:
-                r[i] -= A[i, j] @ new_velocity[j]
+    for i,m in ti.ndrange(n,num_ptr[None]) :
+        j = adj_ptr[i,m]
+        if j >= 0 :
+            r[i] -= A[i, j] @ new_velocity[j]
                 
+    for i in range(n):
         # new_velocity[i] = A[i, i].inverse() @ r[i]
         new_dv[i] = A[i, i].inverse() @ r[i]
         # new_dv[i].x = (r[i].x - A[i,i][0,1] * new_velocity[i].y) / A[i, i][0,0]
@@ -74,9 +82,14 @@ def resi() -> ti.f32:
     res = 0.0
     
     for i in range(n):
-        r[i] = b[i] * 1.0
-        for j in range(n):
+        r[i] = b[i] * 1.0 - A[i,i] @ new_velocity[i]
+
+    for i,m in ti.ndrange(n,num_ptr[None]) :
+        j = adj_ptr[i,m]
+        if j >= 0 :
             r[i] -= A[i, j] @ new_velocity[j]
+
+    for i in range(n):
         res += r[i].x ** 2 
         res += r[i].y ** 2
 
@@ -88,15 +101,15 @@ def residual() -> ti.f32:
 
 @ti.func
 def solve_equation() :
-    # for no_loop in range(1) :
-    for i in range(1000) :
-        iterate()
-        # print("----->----->",resi())
-        resi_out[0] = resi()
-        resi_out[1] = i
-        # print("----->----->",resi_out[0])
-        if resi_out[0] < 1e-7 :
-            break
+    for no_loop in range(1) :
+        for i in range(1000) :
+            iterate()
+            # print("----->----->",resi())
+            resi_out[0] = resi()
+            resi_out[1] = i
+            # print("----->----->",resi_out[0])
+            if resi_out[0] < 1e-5 :
+                break
 
 @ti.kernel
 def substep_explict():
@@ -205,7 +218,7 @@ def substep_jacobi():
 
     # calc force
     for i in range(n) :
-        new_force[i] = gravity * particle_mass[None]
+        new_force[i] = gravity * particle_mass[None] 
         for j in range(n) :
             if rest_length[i,j] != 0 :
                 new_force[i] += f_ij(i,j) 
@@ -254,16 +267,18 @@ def substep_jacobi():
 @ti.func
 def substep_jacobi_semi():
     n = num_particles[None]
+    n_p = num_ptr[None]
     ht2 = ht * ht
 
     # calc force
     for i in range(n) :
-        new_force[i] = gravity * particle_mass[None]
-        for j in range(n) :
-            if rest_length[i,j] != 0 :
-                new_force[i] += f_ij(i,j) 
+        new_force[i] = gravity * particle_mass[None] + out_force[i]
+    for i,m in ti.ndrange(n,n_p) :
+        j = adj_ptr[i,m]
+        if j >= 0 :
+            new_force[i] += f_ij(i,j) 
 
-    collide_box()
+    # collide_box()
 
     # init new velocity
     for i in range(n) :
@@ -274,22 +289,27 @@ def substep_jacobi_semi():
 
     # fill A b
     for i in range(n) :
-        for j in range(n) :
+        A[i,i] *= 0.0
+    for i,m in ti.ndrange(n,n_p) :
+        j = adj_ptr[i,m]
+        if j >= 0 :
             A[i,j] *= 0.0
 
     for i in range(n) :
         A[i,i] += mass
-        for j in range(n) :
-            if rest_length[i, j] != 0:
-                A[i,i] += ht2 * dfj_ij(i,j)
-                A[i,j] -= ht2 * dfj_ij(i,j)
+    for i,m in ti.ndrange(n,n_p) :
+        j = adj_ptr[i,m]
+        if j >= 0 :
+            A[i,i] += ht2 * dfj_ij(i,j)
+            A[i,j] -= ht2 * dfj_ij(i,j)
 
     for i in range(n) :
         b[i] = new_force[i] * ht * 2 + mass @ velocity[i]
-        for j in range(n) :
-            if rest_length[i,j] != 0 :
-                b[i] -= ht2 * dfj_ij(i,j) @ velocity[i] 
-                b[i] += ht2 * dfj_ij(i,j) @ velocity[j] 
+    for i,m in ti.ndrange(n,n_p) :
+        j = adj_ptr[i,m]
+        if j >= 0 :
+            b[i] -= ht2 * dfj_ij(i,j) @ velocity[i] 
+            b[i] += ht2 * dfj_ij(i,j) @ velocity[j] 
                 
     # solve equation for new velocity
     solve_equation()
@@ -304,7 +324,7 @@ def substep_jacobi_semi():
 @ti.kernel
 def step_jacobi():
     for no_loop in range(1) :
-        for step in range(1):
+        for step in range(100):
             substep_jacobi_semi()
             # substep_jacobi()
         
@@ -329,7 +349,15 @@ def hit_particle(pos_x: ti.f32, pos_y: ti.f32):
         # dist = (position[i] - ti.Vector([pos_x,pos_y])).norm()
         dist = abs(position[i].y - pos_y)
         if dist < 0.05 and position[i].y > bottom_y :
-            position[i] += ti.Vector([dist_x,0.0])
+            out_force[i] += ti.Vector([dist_x,0.0]) * 30
+            # position[i] += ti.Vector([dist_x,0.0])
+
+@ti.kernel
+def hit_clear(): 
+    n = num_particles[None]
+    for i in range(n) :
+        out_force[i] *= 0
+
 
 @ti.kernel
 def conn_particle(pos_i: ti.i32, pos_j: ti.i32) :
@@ -337,15 +365,13 @@ def conn_particle(pos_i: ti.i32, pos_j: ti.i32) :
     rest_length[pos_i, pos_j] = dist # 0.1
     rest_length[pos_j, pos_i] = dist # 0.1
     
-gui = ti.GUI('Mass Spring System', res=(512, 512), background_color=0xdddddd)
-
 
 def init() :
-    n_xs = 2
-    n_ys = 8
+    n_xs = 4
+    n_ys = 40
     for i in range(n_xs) :
         for j in range(n_ys) :
-            new_particle(0.25 + i * 0.1, 0.00 + bottom_y + j * 0.1)
+            new_particle(0.35 + i * 0.02, 0.00 + bottom_y + j * 0.02)
             if i > 0 :
                 conn_particle(i*n_ys+j,(i-1)*n_ys+(j+0))
             if j > 0 :
@@ -353,6 +379,17 @@ def init() :
             if i > 0 and j > 0 :
                 conn_particle(i*n_ys+j,(i-1)*n_ys+(j-1))
                 conn_particle((i-0)*n_ys+(j-1),(i-1)*n_ys+(j-0))
+
+    n = num_particles[None]
+    for i in range(n) :
+        m = 0
+        for j in range(n) :
+            if rest_length[i,j] != 0 :
+                adj_ptr[i,m] = j
+                m += 1
+        while m < num_ptr[None] :
+            adj_ptr[i,m] = -1
+            m += 1
 
 init()
 # new_particle(0.3, 0.2)
@@ -379,6 +416,8 @@ def debug_without_gui() :
 
 
 # debug_without_gui()
+
+gui = ti.GUI('Mass Spring System', res=(512, 512), background_color=0xdddddd)
 
 while True:
     for e in gui.get_events(ti.GUI.PRESS):
@@ -407,20 +446,37 @@ while True:
                 
     if not paused[None]:
         step_jacobi()
+        hit_clear()
         print('resi out : ',resi_out[0],resi_out[1])
-        print(f'residual={residual():0.10f}')
+        # print(f'residual={residual():0.10f}')
     
     X = position.to_numpy()
-    gui.circles(X[:num_particles[None]], color=0xffaa77, radius=8)
+    Y = rest_length.to_numpy()
+    Z = adj_ptr.to_numpy()
+    gui.circles(X[:num_particles[None]], color=0xffaa77, radius=1)
     
     gui.line(begin=(0.0, bottom_y), end=(1.0, bottom_y), color=0x0, radius=1)
     
     for i in range(num_particles[None]):
-        for j in range(i + 1, num_particles[None]):
-            if rest_length[i, j] != 0:
-                gui.line(begin=X[i], end=X[j], radius=2, color=0x445566)
+        #for j in range(i + 1, num_particles[None]):
+        #    if rest_length[i, j] != 0:
+        #        gui.line(begin=X[i], end=X[j], radius=2, color=0x445566)
+        for j in Z[i] :
+            if j >= 0 :
+                dist = math.sqrt((X[i][0]-X[j][0])**2 + (X[i][1]-X[j][1])**2)
+                ratio = (dist - Y[i][j])/Y[i][j]
+                ratio *= 100
+                ratio = max(-1.0, ratio)
+                ratio = min( 1.0, ratio)
+                if ratio < 0 :
+                    color = int(-ratio * 255)
+                    color *= 0x10000
+                else :
+                    color = int( ratio * 255)
+                gui.line(begin=X[i], end=X[j], radius=1.5, color=color + 0x000000 )
     gui.text(content=f'C: clear all; Space: pause', pos=(0, 0.95), color=0x0)
     gui.text(content=f'S: Spring stiffness {spring_stiffness[None]:.1f}', pos=(0, 0.9), color=0x0)
     gui.text(content=f'D: damping {damping[None]:.2f}', pos=(0, 0.85), color=0x0)
     gui.show()
+
 
